356a7d06f085ce447216921f0d0f93d3808dba89
Angel Emanuel Villalonga <88913610+Ema-villalonga@users.noreply.github.com>
2022-05-18 20:50:14 -0300

    Initial commit

---
+++ src/pages/view.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Layout from "../components/layout";
+import { useAppContext } from "../store/store";
+
+export default function View() {
+  const [item, setItem] = useState(null);
+  const params = useParams();
+  const store = useAppContext();
+
+  useEffect(() => {
+    const book = store.getItem(params.bookId);
+    setItem(book);
+  }, []);
+
+  const itemStyles = {
+    container: {
+      display: "flex",
+      gap: "20px",
+      color: "white",
+      width: "800px",
+      margin: "0 auto"
+    }
+  };
+
+  if (!item) {
+    return <Layout>Item not found</Layout>;
+  }
+  return (
+    <Layout>
+      <div style={itemStyles.container}>
+        <div>
+          {item?.cover ? <img src={item?.cover} width="400" alt="" /> : ""}
+        </div>
+        <div>
+          <div>{item?.title}</div>
+          <div>{item?.author}</div>
+          <div>{item?.intro}</div>
+          <div>{item?.completed ? "Leído" : "Por terminar"}</div>
+          <div>{item?.review}</div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
